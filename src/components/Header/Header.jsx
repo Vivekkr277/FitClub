653ddf337017e7310f6d4fdbc8994658ef9ab9ac
vivekkr277 <vkr70611@gmail.com>
2024-06-20 01:21:17 +0530

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.css";
 import Logo from "../../assets/logo.png";
 import Bars from "../../assets/bars.png";
 import { Link } from "react-scroll";
+
 const Header = () => {
   const mobile = window.innerWidth <= 768 ? true : false;
   const [menuOpened, setmenuOpened] = useState(false);
@@ -20,11 +21,41 @@ const Header = () => {
         </div>
       ) : (
         <ul className="header-menu">
-          <a onClick={()=>setmenuOpened(false)} className="anchor-tag" href="#homes">Home</a>
-          <a onClick={()=>setmenuOpened(false)}className="anchor-tag" href="#programs">Program</a>
-          <a onClick={()=>setmenuOpened(false)}className="anchor-tag" href="#reasons">Why us</a>
-          <a onClick={()=>setmenuOpened(false)} className="anchor-tag" href="#plans">Plans</a>
-          <a onClick={()=>setmenuOpened(false)}className="anchor-tag" href="#testimonials">Testimonials</a>
+          <a
+            onClick={() => setmenuOpened(false)}
+            className="anchor-tag"
+            href="#homes"
+          >
+            Home
+          </a>
+          <a
+            onClick={() => setmenuOpened(false)}
+            className="anchor-tag"
+            href="#programs"
+          >
+            Program
+          </a>
+          <a
+            onClick={() => setmenuOpened(false)}
+            className="anchor-tag"
+            href="#reasons"
+          >
+            Why us
+          </a>
+          <a
+            onClick={() => setmenuOpened(false)}
+            className="anchor-tag"
+            href="#plans"
+          >
+            Plans
+          </a>
+          <a
+            onClick={() => setmenuOpened(false)}
+            className="anchor-tag"
+            href="#testimonials"
+          >
+            Testimonials
+          </a>
         </ul>
       )}
     </div>
